d9df4fb83fa8baf857e80795fd19ec18da5d64de
Ualter Azambuja Junior <ualter@users.noreply.github.com>
2016-02-22 10:13:17 -0300

    Update JMSRequestReplySample.java

--- src/main/java/br/com/eai/jms/requestreply/JMSRequestReplySample.java
+++ src/main/java/br/com/eai/jms/requestreply/JMSRequestReplySample.java
@@ -76,7 +76,7 @@ public class JMSRequestReplySample {
 		
 		jmsSample.registerReplier("ReplierApp");*/
 		
-		quickTestConnectionQueue();
+		quickTestJMSConnectionSendReceiveQueueHELLO();
 		
 	}
 	
